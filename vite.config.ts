865b612289071ace1712dda00e60e07b58effa13
JosephCarazoP <josephcarazo56@gmail.com>
2025-11-11 23:50:37 -0600

    Fix vercel config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/joseph-portfolio/',
+  base: '/', // IMPORTANTE: debe ser '/' para Vercel
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
